9e43be55da128dba5e8da6ea1da450de73112e49
Jaco van der Merwe <jacovdm1@gmail.com>
2023-11-30 23:30:23 +1300

    Test

--- script.js
+++ script.js
@@ -12492,6 +12492,7 @@ function getFormattedDate() {
   const dd = String(today.getDate()).padStart(2, '0');
   const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
   const yyyy = today.getFullYear();
+  
   return mm + '/' + dd + '/' + yyyy;
 }
 

14152e8dcc45c1074591e8d8ce1cd3cf65509f48
Jaco van der Merwe <jacovdm1@gmail.com>
2023-11-30 23:23:46 +1300

    test

--- script.js
+++ script.js
@@ -12492,7 +12492,7 @@ function getFormattedDate() {
   const dd = String(today.getDate()).padStart(2, '0');
   const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
   const yyyy = today.getFullYear();
-  return mm + '/' + dd + '/' + yyyy;
+  return dd + '/' + mm + '/' + yyyy;
 }
 
 // Function to display today's word on the webpage
